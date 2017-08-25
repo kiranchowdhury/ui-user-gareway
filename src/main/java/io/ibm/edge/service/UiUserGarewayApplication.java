427c21f1bb5de0e0a1f7ac606ab8ea676ec92d2d
kiranchowdhury <kiranchowdhury@in.ibm.com>
2017-08-26 00:51:04 +0530

    First commit

---
+++ src/main/java/io/ibm/edge/service/UiUserGarewayApplication.java
@@ -0,0 +1,12 @@
+package io.ibm.edge.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiUserGarewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiUserGarewayApplication.class, args);
+	}
+}
